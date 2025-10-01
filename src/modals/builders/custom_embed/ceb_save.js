aec877f752892937006ced67bb95945403daa24f
Jack <hello@jackperry.co>
2025-10-02 00:03:12 +0100

    feat: ✨ save to database system

---
+++ src/modals/builders/custom_embed/ceb_save.js
@@ -0,0 +1,36 @@
+const { ModalSubmitInteraction, EmbedBuilder, MessageFlags } = require('discord.js');
+
+const StatusEmbedBuilder = require('../../../structures/funcs/tools/createStatusEmbed');
+const statusEmbed = new StatusEmbedBuilder("Save", { name: "Embed Builder" });
+
+/** @typedef {import("../../../structures/funcs/util/Types").ExtendedClient} ExtendedClient */
+
+module.exports = {
+    id: "ceb_save",
+
+    /**
+    * 
+    * @param {ModalSubmitInteraction} interaction 
+    * @param {ExtendedClient} client 
+    */
+    async execute(interaction, client) {
+        const name = interaction.fields.getTextInputValue('ceb_save_name_i').trim();
+        const customEmbed = interaction.message.embeds?.[0];
+        if (!customEmbed) return interaction.reply({ embeds: [statusEmbed.create("There was an error fetching the embed, have they been deleted?", 'Red')], flags: [MessageFlags.Ephemeral] });
+
+        const savedEmbed = EmbedBuilder.from(customEmbed).toJSON();
+        const newRecord = await client.db.embed.create({
+            data: {
+                name,
+
+                createdAt: Math.floor(Date.now() / 1000),
+                updatedAt: Math.floor(Date.now() / 1000),
+                createdBy: interaction.user.id,
+
+                embedJson: savedEmbed
+            }
+        });
+
+        return interaction.reply({ embeds: [statusEmbed.create(`Your embed has been saved successfully!\n\n**Embed ID**: ${newRecord.id}\n**Embed Name:** ${newRecord.name}`, 'Green')], flags: [MessageFlags.Ephemeral] });
+    }
+};
