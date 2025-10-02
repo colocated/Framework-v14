de880a59a22516ef182d46234432830a782c7623
Jack <hello@jackperry.co>
2025-10-02 21:55:51 +0100

    fix: 🔧 wrap database save in case of errors

--- src/modals/builders/custom_embed/ceb_save.js
+++ src/modals/builders/custom_embed/ceb_save.js
@@ -19,17 +19,23 @@ module.exports = {
         if (!customEmbed) return interaction.reply({ embeds: [statusEmbed.create("There was an error fetching the embed, have they been deleted?", 'Red')], flags: [MessageFlags.Ephemeral] });
 
         const savedEmbed = EmbedBuilder.from(customEmbed).toJSON();
-        const newRecord = await client.db.embed.create({
-            data: {
-                name,
+        let newRecord;
+        try {
+            newRecord = await client.db.embed.create({
+                data: {
+                    name,
 
-                createdAt: Math.floor(Date.now() / 1000),
-                updatedAt: Math.floor(Date.now() / 1000),
-                createdBy: interaction.user.id,
+                    createdAt: Math.floor(Date.now() / 1000),
+                    updatedAt: Math.floor(Date.now() / 1000),
+                    createdBy: interaction.user.id,
 
-                embedJson: savedEmbed
-            }
-        });
+                    embedJson: savedEmbed
+                }
+            });
+        } catch (err) {
+            console.error(err);
+            return interaction.reply({ embeds: [statusEmbed.create("There was an error saving the embed to the database. Please try again later.", 'Red')], flags: [MessageFlags.Ephemeral] });
+        }
 
         return interaction.reply({ embeds: [statusEmbed.create(`Your embed has been saved successfully!\n\n**Embed ID**: ${newRecord.id}\n**Embed Name:** ${newRecord.name}`, 'Green')], flags: [MessageFlags.Ephemeral] });
     }
