47e088970ede1ce9424890718c4995dce5c717a2
Jack <hello@jackperry.co>
2025-10-04 22:47:13 +0100

    feat: ✨ refactor jobs listener, add support for dangerous and update to new node-cron scheduling

--- src/structures/handlers/loadJobs.js
+++ src/structures/handlers/loadJobs.js
@@ -1,5 +1,5 @@
 const cron = require('node-cron');
-const { cyan } = require('chalk');
+const { cyan, red } = require('chalk');
 
 const Logger = require('../funcs/util/Logger');
 const { loadFiles } = require('../funcs/fileLoader');
@@ -12,17 +12,12 @@ const { loadFiles } = require('../funcs/fileLoader');
  */
 async function loadJobs(client) {
     client.jobs.forEach(job => {
-        try {
-            job.task?.stop();
-        } catch (error) {
-            Logger.error(`[Jobs] Failed to stop ${job.id}: ${error.message}`);
-        }
+        try { job.task?.destroy(); }
+        catch (error) { Logger.error(`[Jobs] Failed to destroy ${job?.id ?? 'unknown job'}: ${error.message}`); }
     });
     client.jobs.clear();
 
-    let jobsArray = [];
-
-    const files = await loadFiles("jobs");
+    const files = await loadFiles("src/jobs");
     if (!files.length) return Logger.warn(`[Jobs] Not preparing. No job files found!`);
 
     files.forEach((file) => {
@@ -33,27 +28,31 @@ async function loadJobs(client) {
             if (missing.length) return Logger.warn(`[Jobs] ${file} is missing required properties: ${missing.join(', ')}`);
         }
 
+        if (client.jobs.find(j => j.id === job.id)) {
+            return Logger.warn(`[Jobs] Duplicate job ID found: ${job.id} in ${file} - skipping duplicate instances.`);
+        }
+
         try {
             const task = cron.schedule(job.schedule, () => job.execute(client), {
-                name: job.name ?? job.id,
-
-                scheduled: true,
+                name: job?.name ?? job.id,
                 timezone: client.config.cronTimezone ?? 'UTC',
 
-                recoverMissedExecutions: job.recoverMissedExecutions ?? false,
-                runOnInit: job.runOnInit ?? false,
+                noOverlap: job?.noOverlap ?? false,
+                maxExecutions: job?.maxExecutions ?? 0,
+                maxRandomDelay: job?.maxRandomDelay ?? 0
             });
 
+            if (job?.runOnStartup) job.execute(client);
             job.task = task;
 
             client.jobs.set(job.id, job);
-            jobsArray.push(job.id);
         } catch (error) {
             Logger.error(`[Jobs] Failed to schedule ${job.id}: ${error.message}`);
-        };
+        }
     });
 
-    Logger.success(`[Jobs] Prepared with ${cyan(`${jobsArray.length} jobs`)}!`);
+    const dangerousJobs = client.jobs.filter(job => job?.dangerous);
+    Logger.success(`[Jobs] Prepared with ${red(`${dangerousJobs.size} dangerous jobs`)} and ${cyan(`${client.jobs.size - dangerousJobs.size} safe jobs`)}!`);
 }
 
 module.exports = { loadJobs };
